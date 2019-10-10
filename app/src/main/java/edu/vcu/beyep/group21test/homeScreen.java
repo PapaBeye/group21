293cd9888464292bb0d48540f66cf492b2a66223
Papa Beye <32583389+PapaBeye@users.noreply.github.com>
2019-10-10 13:40:01 -0400

    added drawables

--- app/src/main/java/edu/vcu/beyep/group21test/homeScreen.java
+++ app/src/main/java/edu/vcu/beyep/group21test/homeScreen.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-
+// coding i flow
 public class homeScreen extends AppCompatActivity {
 
 
