7a4a3af1c68207e456c891bf465915004415e5b8
Papa Beye <32583389+PapaBeye@users.noreply.github.com>
2019-10-10 12:15:35 -0400

    splash screen implemented

---
+++ app/src/main/java/edu/vcu/beyep/group21test/homeScreen.java
@@ -0,0 +1,18 @@
+package edu.vcu.beyep.group21test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class homeScreen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+    }
+}
---
+++ app/src/main/java/edu/vcu/beyep/group21test/splashWelcome.java
@@ -0,0 +1,24 @@
+package edu.vcu.beyep.group21test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashWelcome extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_welcome);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(splashWelcome.this, homeScreen.class);
+                startActivity(splashIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
