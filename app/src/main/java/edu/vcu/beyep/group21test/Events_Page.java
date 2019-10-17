8847d1145c0f3a62cbf43054621767998f10b11e
Zorbeus <artisja2@mymail.vcu.edu>
2019-10-17 10:21:42 -0400

    changed events page jaredbranch
    first commit for branch
    
    changed main nav bar color to vcu primary yellow color
    
    spaced out options/formats for nav bar items
    
    enforced/changed code format added PrepareOption removing event from nav bar, made decleration for syncing with json file method
    
    added some new view items

--- app/src/main/java/edu/vcu/beyep/group21test/Events_Page.java
+++ app/src/main/java/edu/vcu/beyep/group21test/Events_Page.java
@@ -7,53 +7,82 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class Events_Page extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-    private DrawerLayout LayoutDrawer;
+
+    private DrawerLayout layoutDrawer;
     private ActionBarDrawerToggle toggleDrawer;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_events__page);
-        LayoutDrawer =  findViewById(R.id.drawer);
-        toggleDrawer = new ActionBarDrawerToggle(this,LayoutDrawer, R.string.open,R.string.close);
-        LayoutDrawer.addDrawerListener(toggleDrawer);
-        toggleDrawer.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        NavigationView nav_view = findViewById(R.id.nav_drawer);
-        nav_view.setNavigationItemSelectedListener(this);
-    }
 
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events__page);
+
+        layoutDrawer =  findViewById(R.id.drawer);
+        toggleDrawer = new ActionBarDrawerToggle(this, layoutDrawer, R.string.open, R.string.close);
+
+        layoutDrawer.addDrawerListener(toggleDrawer);
+        toggleDrawer.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView nav_view = findViewById(R.id.nav_drawer);
+        nav_view.setNavigationItemSelectedListener(this); 
+
+        onPrepareOptionsMenu( nav_view.getMenu() );
+
+    }
+
+    // For preparing menu options (actionbar) before it is loaded when an activity starts.
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+        MenuItem item = menu.findItem(R.id.Nav_Event);
+        if ( item != null ) item.setVisible(false);
+        else return false;
+
+        return true;
+    }
+
+    // Implemented by Papa Beye.
+    @Override
     public boolean onOptionsItemSelected(MenuItem item){
-        if (toggleDrawer.onOptionsItemSelected(item)){
-            return true;
-        }
+
+        if ( toggleDrawer.onOptionsItemSelected(item) ) return true;
+
         return super.onOptionsItemSelected(item);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        if (menuItem.getItemId() == R.id.Nav_Event){
-            LayoutDrawer.closeDrawers();
-        }
+
         if (menuItem.getItemId()== R.id.Nav_Food){
             Intent intent = new Intent(this , food_swipes.class);
             startActivity(intent);
         }
-
-        if (menuItem.getItemId() == R.id.Nav_create_event){
+        else if (menuItem.getItemId() == R.id.Nav_create_event){
             Intent intent = new Intent(this , Create_Events_Page.class);
             startActivity(intent);
         }
-        if (menuItem.getItemId() == R.id.Nav_HomeScreen){
-            System.out.println("cbig check");
+        else if (menuItem.getItemId() == R.id.Nav_HomeScreen){
             Intent intent = new Intent(this , homeScreen.class);
             startActivity(intent);
         }
+
         return false;
     }
+
+    // This method WILL responsible for retrieving and displaying events onto scroll area.
+    public void getEventListings() {
+
+
+
+        return;
+    }
+
 }
