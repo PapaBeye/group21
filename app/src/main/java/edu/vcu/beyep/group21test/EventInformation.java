5b157f977733112d48f8fbe5b5f6cea3f2ba56e6
katherine <reyesmartinkn@vcu.edu>
2019-10-22 18:46:58 -0400

    events info page implemented

---
+++ app/src/main/java/edu/vcu/beyep/group21test/EventInformation.java
@@ -0,0 +1,77 @@
+package edu.vcu.beyep.group21test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class EventInformation extends AppCompatActivity {
+    Button backme;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_information);
+        backme = findViewById(R.id.backeventlist);
+        backme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        get_json();
+    }
+
+    public void get_json(){
+        String json;
+
+        try{
+
+            InputStream is = getAssets().open("events.json");
+
+            int size = is.available();
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+            is.close();
+
+            json = new String(buffer,"UTF-8");
+            JSONArray jsonarray = new JSONArray(json);
+
+            TextView name = findViewById(R.id.name);
+            TextView description = findViewById(R.id.description);
+            TextView location = findViewById(R.id.location);
+            TextView fee = findViewById(R.id.fee);
+
+            int num = getIntent().getIntExtra("num", 0);
+
+
+            JSONObject jsonobject= (JSONObject) jsonarray.get(num);
+
+            String id = jsonobject.optString("name");
+            String value1 = jsonobject.optString("description");
+            String value2 = jsonobject.optString("location");
+            String value3 = jsonobject.optString("fee");
+
+            name.setText(id);
+            description.setText(value1);
+            location.setText(value2);
+            fee.setText(value3);
+
+        } catch (IOException a){
+            a.printStackTrace();
+        } catch (JSONException a){
+            a.printStackTrace();
+        }
+
+    }
+}
