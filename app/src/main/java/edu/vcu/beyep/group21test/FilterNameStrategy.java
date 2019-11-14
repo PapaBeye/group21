29f7ca6ef9ce80d2f1e7e8a83012aaa10d36ae53
Zorbeus <artisja2@mymail.vcu.edu>
2019-11-14 02:09:42 -0500

    contains filter view type objects

---
+++ app/src/main/java/edu/vcu/beyep/group21test/FilterNameStrategy.java
@@ -0,0 +1,8 @@
+package edu.vcu.beyep.group21test;
+
+// Haven't found a particular reason to use this yet. Will be interface for filtering types.
+public interface FilterNameStrategy {
+
+    public void runSortingAlgorithm();
+
+}
---
+++ app/src/main/java/edu/vcu/beyep/group21test/event_filter_page.java
@@ -0,0 +1,18 @@
+package edu.vcu.beyep.group21test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class event_filter_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_filter_page);
+
+    }
+
+
+}
