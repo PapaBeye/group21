9407f513000696a0e51c6e16d67ff349977c8b52
katherine <reyesmartinkn@vcu.edu>
2019-11-14 12:03:29 -0500

    search feature

--- app/src/main/java/edu/vcu/beyep/group21test/Events_Page.java
+++ app/src/main/java/edu/vcu/beyep/group21test/Events_Page.java
@@ -3,14 +3,20 @@ package edu.vcu.beyep.group21test;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
@@ -21,11 +27,13 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
-public class Events_Page extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class Events_Page extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout layoutDrawer;
     private ActionBarDrawerToggle toggleDrawer;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,20 +48,36 @@ public class Events_Page extends AppCompatActivity implements NavigationView.OnN
         toggleDrawer.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        Button button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
+
+        ListView listView = (ListView) findViewById(R.id.eventslist);
+
+        ArrayList<String> events = new ArrayList<>();
+        events.add("Ramfest");
+        events.add("Fall Fest");
+        events.add("VCU Mens BasketBall vs LSU");
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, events);
+        listView.setAdapter(adapter);
+
+        EditText filter = (EditText) findViewById(R.id.search);
+        filter.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Events_Page.this, EventInformation.class);
-                String num = "num";
-                switch(v.getId()){
-                    case R.id.button1:
-                        intent.putExtra(num, 0);
-                        startActivity(intent);
-                        break;
-                }
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                (Events_Page.this).adapter.getFilter().filter(charSequence);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
+
         NavigationView nav_view = findViewById(R.id.nav_drawer);
         nav_view.setNavigationItemSelectedListener(this);
 
