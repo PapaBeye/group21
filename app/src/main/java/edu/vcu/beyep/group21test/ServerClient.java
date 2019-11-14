34ff716a77ba5b9e8aec5e438a857cc1e6b6655c
Papa Beye <32583389+PapaBeye@users.noreply.github.com>
2019-11-14 00:58:02 -0500

    server implemented storing list

---
+++ app/src/main/java/edu/vcu/beyep/group21test/ServerClient.java
@@ -0,0 +1,83 @@
+package edu.vcu.beyep.group21test;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.os.FileUtils;
+
+import java.io.File;
+import android.os.FileUtils;
+import android.util.Log;
+import android.widget.ProgressBar;
+
+import androidx.core.app.ActivityCompat;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Random;
+
+
+public class ServerClient extends Thread implements SeverInterface{
+    public static homeScreen contxt;
+    public  ServerClient(homeScreen pt){
+       contxt = pt;
+    }
+    @Override
+    public void run(){
+        main();
+    }
+
+    @Override
+    public void main() {
+        try {
+
+            File directory = new File(Environment.DIRECTORY_DOWNLOADS+File.separator+"REvents");
+            if (!directory.exists()){
+                System.out.println(directory.toString());
+                directory.mkdir();
+            }
+            Socket Cls = new Socket("34.68.33.7",80);
+            int numfs = 1;
+            int i = 0;
+            while(i!=numfs) {
+                byte[] contents;
+                byte[] RheartB = new byte[2000];
+                InputStream is = Cls.getInputStream();
+                OutputStream os = Cls.getOutputStream();
+                is.read(RheartB, 0, RheartB.length);
+                String hb = new String(RheartB, StandardCharsets.UTF_8);
+                String[] phb =  hb.split("-");
+                numfs = Integer.parseInt(phb[1]);
+                hb = phb[0];
+                String shb = hb + "-";
+                byte[] heartB = shb.getBytes();
+                int s = Integer.parseInt(hb);
+                contents = new byte[s];
+                System.out.println(s);
+                os.write(heartB, 0, heartB.length);
+                is.read(contents, 0, contents.length);
+                String ev = new String(contents);
+                contxt.Events.add(ev);
+                System.out.println(new String(contents));
+                os.flush();
+                i++;
+
+            }
+            Cls.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
+
--- app/src/main/java/edu/vcu/beyep/group21test/homeScreen.java
+++ app/src/main/java/edu/vcu/beyep/group21test/homeScreen.java
@@ -3,27 +3,41 @@ package edu.vcu.beyep.group21test;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 // coding i flow
 public class homeScreen extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout  LayoutDrawer;
     private ActionBarDrawerToggle toggleDrawer;
+    public static List<String> Events = new ArrayList<String>();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         LayoutDrawer =  findViewById(R.id.drawer);
         toggleDrawer = new ActionBarDrawerToggle(homeScreen.this,LayoutDrawer, R.string.open,R.string.close);
         LayoutDrawer.addDrawerListener(toggleDrawer);
@@ -32,8 +46,11 @@ public class homeScreen extends AppCompatActivity implements NavigationView.OnNa
         NavigationView nav_view = findViewById(R.id.nav_drawer);
         nav_view.setNavigationItemSelectedListener(this);
         onPrepareOptionsMenu(nav_view.getMenu());
+        Thread server = new ServerClient(homeScreen.this);
+        server.start();
 
     }
+
 
     public boolean onPrepareOptionsMenu(Menu menu) {
 
@@ -74,4 +91,5 @@ public class homeScreen extends AppCompatActivity implements NavigationView.OnNa
         }
         return false;
     }
+
 }
